11d499a46eef0527988e8924c13f118e4dc4512c
Myself2903 <hassanbarranco2003123@gmail.com>
2023-04-30 19:41:34 -0500

    primer commit. Primer avance de la pagina principal

---
+++ frontend/src/Pages/MainPage.jsx
@@ -0,0 +1,34 @@
+import logo from '../assets/react.svg'
+import background from '../assets/background.png'
+import '../styles/MainPage.css'
+
+const MainPage = ()=>{
+    return (
+        <>
+            <header>
+                <nav className='navBar'>
+                    <div className="brand">
+                        <img src={logo} alt="logo img" className='logo'/>
+                        <label>BVParking</label>
+                    </div>
+                    
+                    <ul className='navOptions'>
+                        <li><a>Página Principal</a></li>
+                        <li><a>Mapa de Parqueo</a></li>
+                        <li><a>PQR</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <section className='mainContent'>
+                <img src={background} alt="background image" className='backgroundImage'/>
+                <div className="infoDialog">
+                    <h1>PARQUEADERO CENTRO COMERCIAL<br/>BELLAVISTA</h1>
+                    <button className='parkingButton'>Ver mapa del parqueadero</button>
+                    <p>Conoce la disponibilidad del parqueadero del Centro Comercial BellaVista</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default MainPage
